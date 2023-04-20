a9db8879980d278219b1cc7a4be4a3df0df1e238
Fernando Nagase <nagase.fernando@gmail.com>
2023-04-20 17:09:17 -0300

    install and configure prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Site em Construção</title>
-      </Head>
-      <div>
-        <h1>Site em Construção</h1>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Site em Construção</title>
+            </Head>
+            <div>
+                <h1>Site em Construção</h1>
+            </div>
+        </>
+    )
 }
