2bc59ba180808dcf5b403a2877cabe806f44e52c
Fernando Nagase <nagase.fernando@gmail.com>
2023-05-30 20:33:53 -0300

    add link focus styles

--- theme/components/link.ts
+++ theme/components/link.ts
@@ -1,14 +1,31 @@
 import { defineStyleConfig } from '@chakra-ui/react'
 
+const defaultFocusedStyles = {
+    backgroundColor: 'yellow.100',
+    borderBlockStart: '1px solid var(--chakra-colors-gray-400)',
+    borderBlockEnd: '1px solid var(--chakra-colors-gray-400)',
+}
+
+const navigationFocusedStyles = {
+    border: 'none',
+    backgroundColor: 'transparent',
+    color: 'primary.700',
+    textDecoration: 'underline',
+}
+
 export default defineStyleConfig({
     baseStyle: {
         textDecoration: 'underline',
         color: 'primary.400',
+        _hover: defaultFocusedStyles,
+        _focusVisible: defaultFocusedStyles,
     },
     variants: {
         navigation: {
             textDecoration: 'none',
             color: 'fg',
+            _hover: navigationFocusedStyles,
+            _focusVisible: navigationFocusedStyles,
         },
     },
 })
