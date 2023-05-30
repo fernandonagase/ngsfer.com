f34efee9064cf22826e732c5808f7b18ab3c6ce2
Fernando Nagase <nagase.fernando@gmail.com>
2023-05-30 20:13:43 -0300

    add skip-nav link

--- layout/MainLayout/index.tsx
+++ layout/MainLayout/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { Box } from '@chakra-ui/react'
+import { SkipNavLink, SkipNavContent } from '@chakra-ui/skip-nav'
 
 type MainLayoutProps = {
     children: ReactNode
@@ -11,8 +12,10 @@ type MainLayoutProps = {
 export default function MainLayout({ children }: MainLayoutProps) {
     return (
         <>
+            <SkipNavLink zIndex="skipLink">Ir para o conteúdo</SkipNavLink>
             <Header />
             <Box px="baseline-2" my="baseline-4" as="main">
+                <SkipNavContent />
                 {children}
             </Box>
             <Footer />
