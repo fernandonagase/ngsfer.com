4aa60826395de930a765c4677666cac287eb8dc4
Fernando Nagase <nagase.fernando@gmail.com>
2023-05-31 16:27:16 -0300

    limit layout max width

--- layout/MainLayout/index.tsx
+++ layout/MainLayout/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { Box } from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 import { SkipNavLink, SkipNavContent } from '@chakra-ui/skip-nav'
 
 type MainLayoutProps = {
@@ -13,12 +13,14 @@ export default function MainLayout({ children }: MainLayoutProps) {
     return (
         <>
             <SkipNavLink zIndex="skipLink">Ir para o conteúdo</SkipNavLink>
-            <Header />
-            <Box px="baseline-2" my="baseline-4" as="main">
-                <SkipNavContent />
-                {children}
-            </Box>
-            <Footer />
+            <Container maxW="75ch">
+                <Header />
+                <Box px="baseline-2" my="baseline-4" as="main">
+                    <SkipNavContent />
+                    {children}
+                </Box>
+                <Footer />
+            </Container>
         </>
     )
 }
