27f790ccf6dd1afb18d0a0a7e375b0ec7ac071fd
Fernando Nagase <nagase.fernando@gmail.com>
2023-06-17 17:06:33 -0300

    update ProjectCard title styles

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, LinkBox, LinkOverlay, Text } from '@chakra-ui/react'
+import { Box, Heading, Link, Text, useTheme } from '@chakra-ui/react'
 import NextLink from 'next/link'
 
 type ProjectCardProps = {
@@ -12,27 +12,37 @@ export default function ProjectCard({
     name,
     description,
 }: ProjectCardProps) {
+    const theme = useTheme()
+
     return (
         <Box as="article">
-            <LinkBox>
-                <Heading
-                    as="h2"
-                    fontSize={['2xl', null, null, '2xl-lg']}
-                    lineHeight={['baseline-3', null, null, 'baseline-3-lg']}
-                    fontWeight="600"
+            <Heading
+                as="h2"
+                fontSize={['xl', null, null, 'xl-lg']}
+                lineHeight={['baseline-2', null, null, 'baseline-3-lg']}
+                fontWeight="400"
+                marginBottom={['baseline-1', null, null, 'baseline-1-lg']}
+            >
+                <Link
+                    href={`portfolio/${slug}`}
+                    color={theme.colors.fg}
+                    textDecoration="none"
+                    _hover={{
+                        textDecoration: 'underline',
+                        color: theme.colors.primary[700],
+                    }}
+                    as={NextLink}
                 >
-                    <LinkOverlay href={`portfolio/${slug}`} as={NextLink}>
-                        {name}
-                    </LinkOverlay>
-                </Heading>
-                <Text
-                    fontSize={['md', null, null, 'md-lg']}
-                    lineHeight={['baseline-2', null, null, 'baseline-2-lg']}
-                    noOfLines={2}
-                >
-                    {description}
-                </Text>
-            </LinkBox>
+                    {name}
+                </Link>
+            </Heading>
+            <Text
+                fontSize={['md', null, null, 'md-lg']}
+                lineHeight={['baseline-2', null, null, 'baseline-2-lg']}
+                noOfLines={2}
+            >
+                {description}
+            </Text>
         </Box>
     )
 }
